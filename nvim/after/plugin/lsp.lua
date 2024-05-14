6059694e6abecbe064911220df9ef8fdfb1d84bb
lillie <onlinechronically@protonmail.com>
2024-05-14 16:29:41 -0400

    add: more lsp keybinds for neovim

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,7 +3,7 @@ local lsp = require('lsp-zero')
 lsp.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
-    lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({ buffer = bufnr })
 end)
 
 -- to learn how to use mason.nvim
@@ -34,6 +34,17 @@ local autoformat = function(bufnr)
 end
 lsp.on_attach(function(client, bufnr)
     autoformat(bufnr)
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 cmp.setup({
     window = {
@@ -41,14 +52,13 @@ cmp.setup({
         documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-        ["<C-i>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-o>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<C-p>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-u>"] = cmp.mapping.complete()
+        ["<C-n>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-m>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<C-Enter>"] = cmp.mapping.confirm({ select = true })
     }),
     snippet = {
         expand = function(args)
             require('luasnip').lsp_expand(args.body)
         end,
     },
-  })
+})
