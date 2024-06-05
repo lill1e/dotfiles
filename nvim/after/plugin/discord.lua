72d06804ce1337b3b33883c8d72bc5544fb24a02
lillie <onlinechronically@protonmail.com>
2024-06-05 06:55:44 -0400

    add: rich presence to neovim

---
+++ nvim/after/plugin/discord.lua
@@ -0,0 +1,12 @@
+require("neocord").setup({
+    logo             = "auto",
+    logo_tooltip     = nil,
+    main_image       = "language",
+    client_id        = "1157438221865717891",
+    log_level        = nil,
+    debounce_timeout = 10,
+    blacklist        = {},
+    file_assets      = {},
+    show_time        = true,
+    global_timer     = true
+})
--- nvim/lua/lillie/packer.lua
+++ nvim/lua/lillie/packer.lua
@@ -44,4 +44,5 @@ return require('packer').startup(function(use)
     use("lervag/vimtex")
     use { "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" }
     use({ "epwalsh/obsidian.nvim", requires = "nvim-lua/plenary.nvim" })
+    use("IogaMaster/neocord")
 end)
