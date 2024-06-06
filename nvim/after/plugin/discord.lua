d6def9a9cf72631df0d186e06858e5d7fa8f2495
lillie <onlinechronically@protonmail.com>
2024-06-06 06:15:41 -0400

    add: custom file asset for latex in neovim rpc

--- nvim/after/plugin/discord.lua
+++ nvim/after/plugin/discord.lua
@@ -2,11 +2,13 @@ require("neocord").setup({
     logo             = "auto",
     logo_tooltip     = nil,
     main_image       = "language",
-    client_id        = "1157438221865717891",
+    client_id        = "1247941011724959785",
     log_level        = nil,
     debounce_timeout = 10,
     blacklist        = {},
-    file_assets      = {},
+    file_assets      = {
+        tex = { "LaTeX", "tex" }
+    },
     show_time        = true,
     global_timer     = true
 })
