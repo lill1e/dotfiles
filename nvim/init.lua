f4b167f61f876b6c282af6e07a39fafa58d6046f
lillie <onlinechronically@protonmail.com>
2024-06-18 06:33:47 -0400

    add: two way with sys clipboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,7 +14,7 @@ vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
 vim.opt.conceallevel = 2
 
-vim.api.nvim_set_option("clipboard", "unnamed")
+vim.api.nvim_set_option("clipboard", "unnamedplus")
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "markdown",
