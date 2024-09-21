dbb91439df985b13bf4e78d82d2fed05ce350c37
lillie <onlinechronically@protonmail.com>
2024-09-20 20:41:52 -0400

    add: everforest theme

--- scripts/fehbg.sh
+++ scripts/fehbg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ "$MODE" == "LAPTOP" ] || [ "$MODE" == "THINKPAD" ]
 then
-    feh --no-fehbg --bg-fill "/home/lillie/Downloads/cyberpunk.png"
+    feh --no-fehbg --bg-fill "/home/lillie/Downloads/f.png"
 else
     feh --no-fehbg --bg-fill "/home/lillie/Downloads/cyberpunk.png" "/home/lillie/Downloads/jinx.png" "/home/lillie/Downloads/nightroad2.png"
 fi
