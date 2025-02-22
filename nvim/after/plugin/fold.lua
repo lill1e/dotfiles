546121bddd888ebe3b3445b928b531d08b75e8d9
lillie <onlinechronically@protonmail.com>
2025-02-22 18:48:37 -0500

    add: code folding to nvim

--- nvim/after/plugin/fold.lua
+++ nvim/after/plugin/fold.lua
@@ -1,2 +1,5 @@
-vim.o.foldcolumn = "1"
-vim.o.foldenable = true
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevelstart = 99
+vim.opt.foldlevel = 99
+vim.o.foldcolumn = "0"
