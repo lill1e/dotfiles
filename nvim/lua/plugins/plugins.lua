94cfa945aee0ed4e25db7674a7cc2ac8147360cf
lillie <onlinechronically@protonmail.com>
2026-01-15 09:42:33 -0500

    change: theme to atom onedark

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,11 +1,11 @@
 return {
     { "nvim-telescope/telescope.nvim",   tag = "0.1.8",      dependencies = { "nvim-lua/plenary.nvim" } },
     {
-        "catppuccin/nvim",
-        name = "catppuccin",
+        "navarasu/onedark.nvim",
         priority = 1000,
         config = function()
-            vim.cmd("colorscheme catppuccin-frappe")
+            require("onedark").setup({ style = "dark" })
+            vim.cmd("colorscheme onedark")
         end
     },
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
